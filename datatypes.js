82030ffb947e17b91d8c54c34162d75986eb35d0
Shravani Jaiswal <tychephp@gmail.com>
2024-09-26 13:01:42 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // use all the code as the newer version of Javascript
+// alert(3+1) // wont work since we are using on node.js and not chrome console
+
+console.log(4+4)
+// code readiblity should be high
+
+let name = "Hitesh"
+let age = 12
+let isLoggedin = true
+
+// numebr => 2 to the power of 53
+// bigInt 
+// string = " "
+// boolen = true/false
+// null - standalone value ( representation of empty value)
+// undefined ( variable is defined but the value is not assigned by the user)
+// symbol = unique
+
+// object
+
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined 
+
+
---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountmail = "abc@123@GMAIL.COM"
+var accountPassword = "hihih"
+accountcity = "Noida"
+let accountstate;
+
+accountmail = "hello@gmail.com"
+accountPassword = "121212"
+accountcity = "Ghaziabad"
+
+console.log(accountmail);
+console.table([accountId, accountPassword, accountcity , accountstate])
+
+/* THIS IS A COMMENT..
+prefer not to use var, beacuse of issue in block scope and functional scope..
+*/
